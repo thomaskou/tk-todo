3b0ca66526309ad09420f66041923a8612705838
Thomas Kou <thomaskou00@gmail.com>
2019-03-24 20:33:17 -0400

    Finished!

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-#tk-todo
+# tk-todo
 
 A rudimentary todo list created in React.
+
+<img src="/images/1.png" width="256"> <img src="/images/2.png" width="256"> <img src="/images/3.png" width="256"> <img src="/images/4.png" width="256">
--- src/App.js
+++ src/App.js
@@ -6,17 +6,34 @@ class ListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      task: this.props.task
+      id: this.props.id,
+      task: this.props.task,
+      completed: this.props.completed,
+      date: this.props.date
     }
+    if ((this.state.date[4] + "").length == 1) {
+      this.state.date[4] = "0" + this.state.date[4];
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.getClassType = this.getClassType.bind(this);
+  }
+
+  handleClick() {
+   this.props.handleUpdate(this.state.id);
+   this.setState(this.props.getState(this.state.id));
+  }
+
+  getClassType() {
+    return (this.state.completed ? "ListItemDone" : "ListItem");
   }
 
   render() {
     return (
-      <div class="ListItemContainer">
-        <div class="ListItem">
-          <div class="ListItemText">{ this.state.task }</div>
+      <div className="ListItemContainer" onClick={this.handleClick}>
+        <div className={this.getClassType()}>
+          <div className="ListItemText">{ this.state.task }</div>
+          <div className="ListItemDate">Made { this.state.date[1] }/{ this.state.date[2] } at { this.state.date[3] }:{ this.state.date[4] }</div>
         </div>
-        <div class="ListItemBg"></div>
       </div>
     );
   }
@@ -34,10 +51,37 @@ class TodoList extends Component {
     this.setState({itemList: [...this.state.itemList, todo]});
   }
 
+  getItemState = (id) => {
+    var tempList = this.state.itemList;
+    for (var k = 0; k < tempList.length; k++) {
+      if (tempList[k].id == id) {
+        return tempList[k];
+      }
+    }
+    return null;
+  }
+
+  itemCompleted = (id) => {
+    var tempList = this.state.itemList;
+    for (var k = 0; k < tempList.length; k++) {
+      if (tempList[k].id == id) {
+        tempList[k].completed = !(tempList[k].completed);
+        break;
+      }
+    }
+    this.setState({itemList: tempList});
+  }
+
+  removeCompleted = () => {
+    this.setState({
+      itemList: this.state.itemList.filter(todo => todo.completed == false)
+    });
+  }
+
   render() {
     return (
       <div>
-        {this.state.itemList.map( listItem => <ListItem task={listItem.task} /> )}
+        {this.state.itemList.map( listItem => <ListItem key={listItem.id} id={listItem.id} task={listItem.task} completed={listItem.completed} date={listItem.date} handleUpdate={this.itemCompleted} getState={this.getItemState} /> )}
       </div>
     );
   }
@@ -59,8 +103,13 @@ class AddTodo extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.handleSubmit(this.state.value);
-    this.setState({value: ''});
+    if (this.state.value == '') {
+      this.props.handleError("Task must be at least 1 character long.");
+    } else {
+      this.props.handleSubmit(this.state.value);
+      this.props.handleError("");
+      this.setState({value: ''});
+    }
   }
 
   render() {
@@ -76,29 +125,82 @@ class AddTodo extends Component {
 
 }
 
-class App extends Component {
-
-  todoList = null;
+class TodoError extends Component {
 
   constructor(props) {
     super(props);
+    this.state = { str: "" };
+  }
+
+  render() {
+    return (
+      <div id="TodoError"><i>{ this.state.str }</i></div>
+    );
+  }
+
+}
+
+class RemoveCompleted extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div id="RemoveCompleted" onClick={this.props.handleClick}>Remove completed tasks</div>
+    );
+  }
+
+}
+
+class App extends Component {
+
+  todoList = null;
+  todoError = null;
+
+  constructor(props) {
+    super(props);
+    this.idIncrementer = 0;
     this.todoList = React.createRef();
+    this.todoError = React.createRef();
     this.newTodoItem = this.newTodoItem.bind(this);
+    this.addTodoError = this.addTodoError.bind(this);
+    this.removeCompleted = this.removeCompleted.bind(this);
   }
 
   newTodoItem(value) {
-    /*alert('alert: ' + value);*/
+    var d = new Date();
     var newTodo = {
-      task: value
-    }
+      id: this.idIncrementer,
+      task: value,
+      completed: false,
+      date: [d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes()]
+    };
+    this.idIncrementer++;
     this.todoList.current.addTodoItem(newTodo);
+  }
+
+  addTodoError(message) {
+    this.todoError.current.setState({ str: message });
+  }
+
+  addRemoveButton() {
+    return <RemoveCompleted handleClick={this.removeCompleted}/>;
+  }
+
+  removeCompleted() {
+    this.todoList.current.removeCompleted();
   }
 
   render() {
     return (
       <div id="App">
+        <p id="Title">My Todo List</p>
+        {this.addRemoveButton()}
         <TodoList ref={this.todoList} />
-        <AddTodo handleSubmit={this.newTodoItem}/>
+        <AddTodo handleSubmit={this.newTodoItem} handleError={this.addTodoError} />
+        <TodoError ref={this.todoError} />
       </div>
     );
   }
